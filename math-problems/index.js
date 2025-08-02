83e206b0bdae1488b8876dfd1a2bdc402e5d269f
Gufran Khan <gufran3004@gmail.com>
2025-08-02 13:53:35 +0530

    added  some questions in math-problems

--- math-problems/index.js
+++ math-problems/index.js
@@ -110,5 +110,89 @@ function checkPrime(n) {
   }
   return true;
 }
-let ans = checkPrime(67);
-console.log(ans);
+// let ans = checkPrime(67);
+// console.log(ans);
+
+// ---------------------------------------------------------------------------------------
+
+/* Count Primes (Sieve of Eratosthenes) 
+
+function checkPrimeInRange(n) {
+  let isPrime = new Array(n + 1).fill(true);
+  for (let i = 2; i <= Math.floor(Math.sqrt(n)); i++) {
+    if (isPrime[i] == true) {
+      for (let j = i * i; j <= n; j += i) {
+        isPrime[j] = false;
+      }
+    }
+  }
+  for (let i = 2; i < isPrime.length; i++) {
+    if (isPrime[i]) process.stdout.write(i + " ");
+  }
+}
+
+checkPrimeInRange(10);
+
+*/
+
+/* Find  Sqrt(x) without using built in methods 
+
+function findSqrt(n) {
+  let i = 1;
+  for (; i * i <= n; i++) {
+    if (i * i == n) return i;
+  }
+  return i - 1;
+}
+console.log(findSqrt(135))
+
+*/
+
+/* Power(x, n) 
+
+que: Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+ 
+
+Example 1:
+
+Input: x = 2.00000, n = 10
+Output: 1024.00000
+Example 2:
+
+Input: x = 2.10000, n = 3
+Output: 9.26100
+Example 3:
+
+Input: x = 2.00000, n = -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+ 
+
+Constraints:
+
+-100.0 < x < 100.0
+-231 <= n <= 231-1
+n is an integer.
+Either x is not zero or n > 0.
+-104 <= xn <= 104
+
+
+_________________________________________________________________
+
+
+*/
+
+function findPower(x, n) {
+  if (n == 0) return 1.0;
+  return n < 0 ? 1 / temp(x, n) : temp(x, n);
+}
+
+let temp = function (x, n) {
+  if (n == 0) return 1;
+  let ans = temp(x, Math.floor(n / 2));
+  if (n % 2 == 0) return ans * ans;
+  return (ans * ans * x).toFixed(5);
+};
+
+console.log(findPower(2.10000, 3))
